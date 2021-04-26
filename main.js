2a47925537f4a103f7b33c7148306b49fdaf29a1
luckyjone99 <luckyjone99@gmail.com>
2021-04-26 12:17:50 +0000

    Lab 1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Update this constant with your ServiceNow credentials
 const options = {
   url: 'https://dev112342.service-now.com/',
-  username: 'Admin',
+  username: 'admin',
   password: 'Rinty@4572'
 };
 
